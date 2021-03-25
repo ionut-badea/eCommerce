6c481e9309db8dd503aa6f70fb3de1129d4c6c20
ionut Badea <ionut.badea@yazee.me>
2021-03-25 12:41:28 +0200

    cart products

--- components/CartProduct.vue
+++ components/CartProduct.vue
@@ -1,37 +1,22 @@
 <template>
-  <div id="product" class="container has-background-light">
-    <div
-      id="row_1"
-      class="columns is-marginless is-tablet has-text-centered-mobile"
-    >
-      <div id="image" class="column is-3">
-        <img :src="image" :alt="name" class="image" />
+  <div class="container bg-light my-3 p-4">
+    <div class="row">
+      <div class="col-lg-4">
+        <img :src="image" :alt="name" class="img-thumbnail" />
       </div>
-      <div id="name" class="column is-9">
-        <h2 class="title is-size-4 has-text-dark is-marginless">
+      <div class="col-lg-8">
+        <h2 class="title text-center text-lg-start">
           {{ name }}
         </h2>
       </div>
     </div>
-    <div
-      id="row_2"
-      class="columns is-marginless is-tablet has-text-centered-mobile"
-    >
-      <div id="quantity" class="column is-3">
-        <p
-          id="quantity_text"
-          class="content is-marginless is-size-4-mobile is-size-5 has-text-dark"
-        >
-          Quantity:
+    <div class="row d-flex flex-column flex-lg-row">
+      <div class="col col-lg-4 text-center">
+        <p class="fs-4">
+          Quantity: <b>{{ quantity }}</b>
         </p>
-        <b
-          id="quantity_number"
-          class="content is-marginless is-size-4-mobile is-size-5 has-text-dark"
-          >{{ quantity }}</b
-        >
         <button
-          id="decrease_quantity"
-          class="button is-small is-info is-paddingless p-1"
+          class="btn btn-dark btn-sm"
           @click="changeQuantity({ uid, operation: 'decrease' })"
         >
           <span
@@ -42,8 +27,7 @@
           ></span>
         </button>
         <button
-          id="increase_quantity"
-          class="button is-small is-info is-paddingless p-1"
+          class="btn btn-dark btn-sm"
           @click="changeQuantity({ uid, operation: 'increase' })"
         >
           <span
@@ -54,47 +38,35 @@
           ></span>
         </button>
       </div>
-      <div id="actions" class="column is-5 has-text-centered-tablet">
-        <button
-          id="favorites"
-          class="button is-paddingless is-link mx-2 p-2"
-          @click="addToFavorites(uid)"
-        >
+      <div class="col col-lg-4 text-center">
+        <button class="btn btn-light text-danger" @click="addToFavorites(uid)">
           <span
             class="iconify"
             data-icon="bi:heart-fill"
             data-inline="false"
-            data-height="24"
-            data-width="24"
+            data-height="36"
+            data-width="36"
           ></span>
         </button>
         <button
-          id="delete"
-          class="button is-paddingless is-danger mx-2 p-2"
+          class="btn btn-light text-secondary"
           @click="deleteFromCart(uid)"
         >
           <span
             class="iconify"
             data-icon="fluent:delete-24-filled"
             data-inline="false"
-            data-height="24"
-            data-width="24"
+            data-height="44"
+            data-width="44"
           ></span>
         </button>
       </div>
-      <div id="price" class="column is-4 has-text-right-tablet">
-        <p
-          v-if="discount > 0"
-          id="full_price"
-          class="content is-marginless is-size-4-mobile is-size-5-tablet has-text-grey"
-        >
+      <div class="col col-lg-4 text-center text-lg-end fs-3">
+        <p v-if="discount > 0" class="text-decoration-line-through">
           ${{ price.toString().split('.')[0] }}
           <sup>{{ price.toFixed(2).toString().split('.')[1] }}</sup>
         </p>
-        <p
-          id="discounted_price"
-          class="content is-marginless is-size-3-mobile is-size-4-tablet has-text-dark"
-        >
+        <p>
           ${{ (price - (price * discount) / 100).toString().split('.')[0] }}
           <sup>
             {{
@@ -151,86 +123,4 @@ export default {
 }
 </script>
 
-<style scoped>
-#quantity {
-  display: grid;
-}
-#quantity_text {
-  grid-area: text;
-}
-#quantity_number {
-  grid-area: number;
-}
-#decrease_quantity {
-  grid-area: decrease;
-}
-#increase_quantity {
-  grid-area: increase;
-}
-#full_price {
-  text-decoration: line-through;
-}
-@media only screen and (max-width: 768px) {
-  #product {
-    border-radius: 3rem;
-  }
-  img.image {
-    border-radius: 1.5rem;
-  }
-  #row_1,
-  #row_2 {
-    padding: 3rem;
-  }
-  #row_1 {
-    padding-bottom: 0.75rem !important;
-  }
-  #row_2 {
-    padding-top: 0.75rem !important;
-  }
-  #quantity {
-    grid-template-areas: '. text number decrease increase .';
-    grid-template-columns: 1fr 1fr 3rem 3rem 3rem 1fr;
-    align-items: center;
-    justify-items: center;
-  }
-  #increase_quantity,
-  #decrease_quantity {
-    border-radius: 0.25rem;
-  }
-}
-@media only screen and (min-width: 769px) {
-  #product {
-    border-radius: 1.5rem;
-  }
-  img.image {
-    border-radius: 0.75rem;
-  }
-  #row_1,
-  #row_2 {
-    padding: 1rem;
-  }
-  #row_1 {
-    padding-bottom: 0rem !important;
-  }
-  #row_2 {
-    padding-top: 0rem !important;
-  }
-  #quantity {
-    grid-template-areas:
-      'text number'
-      'decrease increase';
-    grid-template-columns: 1fr 4rem;
-    align-items: center;
-    justify-items: center;
-  }
-  #quantity_text,
-  #decrease_quantity {
-    justify-self: end !important;
-  }
-
-  #increase_quantity,
-  #decrease_quantity {
-    border-radius: 0.25rem;
-  }
-}
-</style>
+<style scoped></style>
